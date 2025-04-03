6e0c8aba6b07d1d13303c454002bebf2e059e287
nkorporaal <nap.korporaal+github@gmail.com>
2025-04-03 10:30:50 +0200

    ci: step summary

--- scripts/generate-step-summary.js
+++ scripts/generate-step-summary.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const { processFile } = require('./process-cucumber-file');
 
-const outputFile = path.join(__dirname, './../test-reports/cucumber-js/historie/step-summary.txt');
+const outputFile = path.join(__dirname, './../test-reports/cucumber-js/step-summary.txt');
 fs.writeFileSync(outputFile, '', 'utf8');
 
 const fileMap = new Map([
     ["./../test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt", "docs (zonder integratie)"],
-    ["./../test-reports/cucumber-js/historie/test-result-summary.txt", "verblijfplaatshistorie raadplegen"],
+    ["./../test-reports/cucumber-js/historie/test-result-peildatum-summary.txt", "raadpleeg verblijfplaats met peildatum"],
+    ["./../test-reports/cucumber-js/historie/test-result-periode-summary.txt", "raadpleeg verblijfplaats met periode"],
 ]);
 
 fileMap.forEach((caption, filePath) => {
--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -17,9 +17,15 @@ npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-
 
 
 npx cucumber-js -f json:./test-reports/cucumber-js/historie/test-result.json \
-                -f summary:./test-reports/cucumber-js/historie/test-result-summary.txt \
+                -f summary:./test-reports/cucumber-js/historie/test-result-peildatum-summary.txt \
                 -f summary \
                 features/raadpleeg-verblijfplaats-met-peildatum \
+                --tags "not @skip-verify" \
+                --world-parameters "$PARAMS"
+
+npx cucumber-js -f json:./test-reports/cucumber-js/historie/test-result.json \
+                -f summary:./test-reports/cucumber-js/historie/test-result-periode-summary.txt \
+                -f summary \
                 features/raadpleeg-verblijfplaats-met-periode \
                 --tags "not @skip-verify" \
                 --world-parameters "$PARAMS"
